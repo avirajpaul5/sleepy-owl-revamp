53a5ce9b2ddea8549c1d7a2d925c38335ae8f871
NachiketaTiwari <nachiketatiwari56@gmail.com>
2024-06-22 20:36:48 +0530

    added components to root

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
-import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
+import Recog from "./Components/Recog";
+import About from "./Components/About";
+import BestSellers from "./Components/BestSellers";
+import Products from "./Components/Products";
+import Merch from "./Components/Merch";
+import Reviews from "./Components/Reviews";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
@@ -11,8 +18,13 @@ const App = () => {
         <Navbar />
         <Hero />
         {/* Add components here ⬇️ */}
-
-        
+        <Recog/>
+        <About/>
+        <BestSellers/>
+        <Products/>
+        <Merch/>
+        <Reviews/>
+        <Footer/>
       </div>
     </ReactLenis>
     // Smooth Scrolling (Don't Touch)
--- src/Components/Recog.js
+++ src/Components/Recog.js
@@ -3,6 +3,8 @@ import React from 'react'
 const Recog = () => {
   return (
     <div>Recog</div>
+    //brands and companies recognized by the app
+    
   )
 }
 
