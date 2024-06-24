984478cfe5db1e949716c3c55504dddb41300cb5
adityaks-11 <adityakrsingh264@gmail.com>
2024-06-24 10:40:42 +0530

    initial navbar commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import "../Styles/Navbar.css"
+import logo from '../assets/Images/navbar-images/sleepy-owl-Photoroom_1.png'
+import search from '../assets/Images/navbar-images/search.png'
+import cart from '../assets/Images/navbar-images/cart.png'
+import arrow from '../assets/Images/navbar-images/sort-down.png'
+
 const Navbar = () => {
   return (
-    <div className ="navbarContainer">Navbar Goes Here! </div>
+    <div className ="navbarContainer">
+      <ul>
+        <li>Shop<img src={arrow}></img></li>
+        <li>Instant Coffee<img src={arrow}></img></li>
+        <li>BestSellers</li>
+      </ul> 
+      <img src={logo} alt="logo" className='logo'></img>
+      <ul><li className='bulk-orders'>BulkOrders</li></ul>
+      <div className='search-box'>
+        <input type='text' placeholder='Search'></input>
+        <img src={search} alt='search-icon'></img>
+      </div>
+      <div className='cart-div'>
+        <img src={cart} alt='cart-icon' className='cart-icon'></img>
+      </div>
+    </div>
   )
 }
 
