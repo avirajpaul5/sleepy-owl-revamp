5e3d6c766d4d8421529f4dff370ca1a8e0725f76
adityaks-11 <adityakrsingh264@gmail.com>
2024-07-03 21:19:08 +0530

    updated BestSellers Product Cards

--- src/Components/BestSellers.js
+++ src/Components/BestSellers.js
@@ -3,6 +3,13 @@ import "../Styles/BestSellers.css";
 import firstproduct from "../assets/Images/eleventh.png";
 import secondproduct from "../assets/Images/fourth.png";
 import thirdproduct from "../assets/Images/tenth.png";
+import coldBrew1 from "../assets/Images/coldBrew1.png";
+import coldBrew2 from "../assets/Images/coldBrew2.png";
+import coldBrew3 from "../assets/Images/coldBrew3.png";
+import hotBrew1 from "../assets/Images/hotBrew1.png";
+import hotBrew2 from "../assets/Images/hotBrew2.png";
+import hotBrew3 from "../assets/Images/hotBrew3.png";
+
 import { Link } from "react-router-dom";
 
 const BestSellers = () => {
@@ -34,48 +41,48 @@ const BestSellers = () => {
     ],
     "cold-brew": [
       {
-        img: thirdproduct,
-        title: "French Vanilla Instant Coffee",
+        img: coldBrew1,
+        title: "Original Blend",
         description:
-          "Made with 100% Arabica solube crystals, get perfect & super delicious coffee in seconds",
-        price: "₹550.00",
+          "Our original 100% Arabica Coffee made to be brewed overnight.",
+        price: "₹600.00",
       },
       {
-        img: secondproduct,
-        title: "Premium Combo",
+        img: coldBrew2,
+        title: "French Vanilla Cold Brew",
         description:
-          "Made with 100% Arabica solube crystals, get perfect & super delicious coffee in seconds",
-        price: "₹1599.00",
+          "A hint of natural sweetness that comes from Vanilla",
+        price: "₹600.00",
       },
       {
-        img: firstproduct,
-        title: "Original Instant Coffee",
+        img: coldBrew3,
+        title: "Hazelnut",
         description:
-          "Made with 100% Arabica solube crystals, get perfect & super delicious coffee in seconds",
-        price: "₹550.00",
+          "Our Signature 100% Arabica with nutty Hazelnut flavour - our personal favourite!",
+        price: "₹600.00",
       },
     ],
     "hot-brew": [
       {
-        img: firstproduct,
-        title: "French Vanilla Instant Coffee",
+        img: hotBrew1,
+        title: "Set of 30 Hot Brew",
         description:
-          "Made with 100% Arabica solube crystals, get perfect & super delicious coffee in seconds",
-        price: "₹550.00",
+          "Enjoy all our flavours in our effortless Hot Brew bags. The perfect gift box for every coffee lover.",
+        price: "₹1200.00",
       },
       {
-        img: secondproduct,
-        title: "Premium Combo",
+        img: hotBrew2,
+        title: "Assorted Hot Brew",
         description:
-          "Made with 100% Arabica solube crystals, get perfect & super delicious coffee in seconds",
-        price: "₹1599.00",
+          "A collection of our most loved 100% Arabica with nutty Hazelnut flavour",
+        price: "₹400.00",
       },
       {
-        img: thirdproduct,
-        title: "Original Instant Coffee",
+        img: hotBrew3,
+        title: "Original",
         description:
-          "Made with 100% Arabica solube crystals, get perfect & super delicious coffee in seconds",
-        price: "₹550.00",
+          "100% Arabica Coffee. A well balanced coffee bag that's ready in 5 minutes. Just add hot water.",
+        price: "₹400.00",
       },
     ],
   };
