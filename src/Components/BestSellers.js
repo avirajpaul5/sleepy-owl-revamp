e9a9e6eed64169fcdf671845d5d9d2e38b929747
adityaks-11 <adityakrsingh264@gmail.com>
2024-07-07 01:30:50 +0530

    Developed New Footer

--- src/Components/BestSellers.js
+++ src/Components/BestSellers.js
@@ -22,21 +22,21 @@ const BestSellers = () => {
         title: "French Vanilla Instant Coffee",
         description:
           "Made with 100% Arabica solube crystals, get perfect & super delicious coffee in seconds",
-        price: "₹550.00",
+        price: "₹550",
       },
       {
         img: secondproduct,
         title: "Premium Combo",
         description:
           "Made with 100% Arabica solube crystals, get perfect & super delicious coffee in seconds",
-        price: "₹1599.00",
+        price: "₹1599",
       },
       {
         img: thirdproduct,
         title: "Original Instant Coffee",
         description:
           "Made with 100% Arabica solube crystals, get perfect & super delicious coffee in seconds",
-        price: "₹550.00",
+        price: "₹550",
       },
     ],
     "cold-brew": [
@@ -45,21 +45,21 @@ const BestSellers = () => {
         title: "Original Blend",
         description:
           "Our original 100% Arabica Coffee made to be brewed overnight.",
-        price: "₹600.00",
+        price: "₹600",
       },
       {
         img: coldBrew2,
         title: "French Vanilla Cold Brew",
         description:
           "A hint of natural sweetness that comes from Vanilla",
-        price: "₹600.00",
+        price: "₹600",
       },
       {
         img: coldBrew3,
         title: "Hazelnut",
         description:
           "Our Signature 100% Arabica with nutty Hazelnut flavour - our personal favourite!",
-        price: "₹600.00",
+        price: "₹600",
       },
     ],
     "hot-brew": [
@@ -68,21 +68,21 @@ const BestSellers = () => {
         title: "Set of 30 Hot Brew",
         description:
           "Enjoy all our flavours in our effortless Hot Brew bags. The perfect gift box for every coffee lover.",
-        price: "₹1200.00",
+        price: "₹1200",
       },
       {
         img: hotBrew2,
         title: "Assorted Hot Brew",
         description:
           "A collection of our most loved 100% Arabica with nutty Hazelnut flavour",
-        price: "₹400.00",
+        price: "₹400",
       },
       {
         img: hotBrew3,
         title: "Original",
         description:
           "100% Arabica Coffee. A well balanced coffee bag that's ready in 5 minutes. Just add hot water.",
-        price: "₹400.00",
+        price: "₹400",
       },
     ],
   };
@@ -138,9 +138,9 @@ const BestSellers = () => {
               <div className="product">
                 <img src={product.img} alt={product.title} />
                 <h2>{product.title}</h2>
-                <p>{product.description}</p>
+                <p style={{color:"#A2A2A2"}}>{product.description}</p>
                 <div className="priceAndAdd">
-                  <h1>{product.price}</h1>
+                  <h1>{product.price}<span style={{color:"#A2A2A2" ,fontSize:"20px"}}>.00</span></h1>
                   <h5>
                     <u>+ADD</u>
                   </h5>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,61 +1,27 @@
 import React from "react";
 import "../Styles/Footer.css";
-import logo from "../assets/Images/navbar-images/sleepy-owl-Photoroom_1.png";
-import LoveLogo from "../assets/Images/Footer-images/Group 10.png";
+import vector from "../assets/Images/Footer-images/Vector.png";
+import footerimg from "../assets/Images/Footer-images/Group 10.png";
 const Footer = () => {
   return (
     <>
-      <div className="footerContainer">
-        <div className="left">
-          <div className="upper">
-            <div className="col">
-              <ul>
-                <li className="itemHeader">Coffee</li>
-                <li className="item">Incredible Deal</li>
-                <li className="item">Premium Instant</li>
-                <li className="item">Cold Brew</li>
-                <li className="item">5-Minute Bag</li>
-                <li className="item">Arabica Beans</li>
-                <li className="item">Ready to Drink</li>
-              </ul>
-            </div>
-            <div className="col">
-              <ul>
-                <li className="itemHeader">Sleepy Owl</li>
-                <li className="item">Reviews</li>
-                <li className="item">About Us</li>
-                <li className="item">Sustain Pack</li>
-                <li className="item">Return</li>
-                <li className="item">Contact-us</li>
-                <li className="item">Terms of Use</li>
-                <li className="item">Compliance</li>
-                <li className="item">Blog</li>
-                <li className="item">Sitemap</li>
-              </ul>
-            </div>
-          </div>
-          <div className="lower">
-            <img src={logo} alt="logo" className="logo"></img>
-          </div>
+      <div className="mainFooterContainer">
+        <div className="leftFooterContainer">
+          <h3>Email Newsletter</h3>
+          <hr></hr>
+          <h1>SLEEPY OWL</h1>
+          <p>Our journey started with three friends with a shared vision. It was simple- introduce people to real good coffee.</p>
         </div>
-        <div className="right">
-          <div className="rightupper">
-            <h1 className="rightText">
-              Our journey started with three friends with a shared vision. It
-              was simple- introduce people to real good coffee. Today, we’re
-              making high-quality, freshly roasted coffee accessible to everyone
-              with our exclusive products.
-            </h1>
+        <div className="rightFooterContainer">
+          <div className="socialButtons">
+            <button>Twitter</button>
+            <button>Instagram</button>
+            <button>Facebook</button>
+            <button>LinkedIn</button>
           </div>
-          <div className="rightmiddle">
-            <div className="search-box">
-              <input type="text" placeholder="Enter Email"></input>
-              <button className="subscribe">Subscribe</button>
-            </div>
-          </div>
-          <div className="rightbottom">
-            <p className="loveMsg">Made with love</p>
-            <img src={LoveLogo} alt="love-logo" className="love-logo"></img>
+          <div className="footerimg">
+            <img src={footerimg} alt="footer-img"></img>
+            <p>Made with love by</p>
           </div>
         </div>
       </div>
