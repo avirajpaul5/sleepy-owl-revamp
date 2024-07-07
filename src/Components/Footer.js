0f0f75b01adba6bd11144fb8f6c2d41738c75e11
Aviraj Paul <avirajpaul5@gmail.com>
2024-07-07 23:22:24 +0530

    fixed footer and responsiveness

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,59 +4,57 @@ import footerimg from "../assets/Images/Footer-images/Group 10.png";
 
 const Footer = () => {
   return (
-    <>
-      <div className="mainFooterContainer">
-        <div className="leftFooterContainer">
-          <h3>Email Newsletter</h3>
-          <hr></hr>
-          <h1>SLEEPY OWL</h1>
-          <p>
-            Our journey started with three friends with a shared vision. It was
-            simple- introduce people to real good coffee.
-          </p>
+    <div className="mainFooterContainer">
+      <div className="leftFooterContainer">
+        <input type="email" placeholder="Email Newsletter"></input>
+        {/* <hr /> */}
+        <h1>SLEEPY OWL</h1>
+        <p>
+          Our journey started with three friends with a shared vision. It was
+          simple- introduce people to real good coffee.
+        </p>
+      </div>
+      <div className="rightFooterContainer">
+        <div className="socialButtons">
+          <a
+            className="socialslink"
+            href="https://twitter.com/sleepyowlcoffee"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>Twitter</button>
+          </a>
+          <a
+            className="socialslink"
+            href="https://www.instagram.com/sleepyowlcoffee"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>Instagram</button>
+          </a>
+          <a
+            className="socialslink"
+            href="https://www.facebook.com/sleepyowlcoffee"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>Facebook</button>
+          </a>
+          <a
+            className="socialslink"
+            href="https://www.youtube.com/channel/UCOC4AAvD10lxvVdnkFW51KQ"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button>Youtube</button>
+          </a>
         </div>
-        <div className="rightFooterContainer">
-          <div className="socialButtons">
-            <div className="firstTwoBtn">
-              <a
-                className="socialslink"
-                href="https://twitter.com/sleepyowlcoffee"
-                target="_blank"
-              >
-                <button>Twitter</button>
-              </a>
-              <a
-                className="socialslink"
-                href="https://www.instagram.com/sleepyowlcoffee"
-                target="_blank"
-              >
-                <button>Instagram</button>
-              </a>
-            </div>
-            <div className="secondTwoBtn">
-              <a
-                className="socialslink"
-                href="https://www.facebook.com/sleepyowlcoffee"
-                target="_blank"
-              >
-                <button>Facebook</button>
-              </a>
-              <a
-                className="socialslink"
-                href="https://www.youtube.com/channel/UCOC4AAvD10lxvVdnkFW51KQ"
-                target="_blank"
-              >
-                <button>Youtube</button>
-              </a>
-            </div>
-          </div>
-          <div className="footerimg">
-            <img src={footerimg} alt="footer-img"></img>
-            <p>Made with love by</p>
-          </div>
+        <div className="footerimg">
+          <img src={footerimg} alt="footer-img" />
+          <p>Made with love by</p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
